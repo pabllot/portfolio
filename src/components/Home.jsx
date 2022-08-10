3bd0c1571dacf57819b0f9f99d9c78811b53dda9
Pabllot07 <pabllot07@outlook.com>
2022-08-10 16:21:34 -0300

    some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <p className='text-pink-600'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Pablo Teixeira</h1>
             <h2 className='text-4xl sm:text=7xl font-bold text-[#8892b0]'>I'm a full Stack Developer</h2>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack dev specializing in building exceptional digital experiences. Currently, I', foccused on building responsive full-stack web applications</p>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>Full-stack dev specializing in building exceptional digital experiences. Currently, I'm foccused on building responsive full-stack web applications</p>
             <div>
                 <button className='text-white text-2xl group border-2 px-3 py-2 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
                   <Link to='work' smooth={true} duration={500}>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
     const handleClick = () => setHamburguer(!hamburguer)
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[70px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <img src={Logo} alt='Logo' style={{width: '90px'}}/>
+            <img src={Logo} alt='Logo' style={{width: '50px'}}/>
         </div>
 
         {/* menu */}
