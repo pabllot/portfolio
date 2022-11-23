28c2c72bdc36325bbe4cb6dea562adbc887d67ef
Pabllot07 <pabllot07@outlook.com>
2022-11-23 18:46:41 -0300

    responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { FaArrowRight } from 'react-icons/fa'
 import { Link } from 'react-scroll'
 import ReactImg from '../assets/test.png';
-import './styles.css'
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import Typescript from '../assets/typescript.svg';
+
+
 
 
 const Home = () => {
@@ -10,9 +15,10 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
         {/* container */}
         <img className='react' src={ReactImg} alt='react icon wrapper'/>
-        <img className='reactsmall' src={ReactImg} alt='react icon wrapper'/>
-        <img className='reactsmall1' src={ReactImg} alt='react icon wrapper'/>
-        <img className='reactsmall2' src={ReactImg} alt='react icon wrapper'/>
+        <img className='js' src={JavaScript} alt='react icon wrapper'/>
+        <img className='html' src={HTML} alt='react icon wrapper'/>
+        <img className='css' src={CSS} alt='react icon wrapper'/>
+        <img className='ts' src={Typescript} alt='react icon wrapper'/>
         <div className=' max-w-[1000px]  mx-auto px- flex flex-col justify-center h-full '>
             <p className='text-pink-600 homep'>Hi, welcome to my portfolio. My name is</p>
             <h1 className='homeh1 text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Pablo Teixeira</h1>
