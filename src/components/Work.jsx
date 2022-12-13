3aba0829641fef18bebaa0444a318ff25f2f1d7f
Pabllot07 <pabllot07@outlook.com>
2022-12-13 11:21:59 -0300

    taking out series

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -91,24 +91,8 @@ const Work = () => {
                         </div>
                     </div>
                 </div>
-                <div style={{backgroundImage: `url(${series})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                            Full Stack Series App
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a target='_blank' rel="noreferrer" href='https://favseriesapp.vercel.app/'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a target='blank' rel="noreferrer" href='https://github.com/pabllot/favseriesfrontend'>
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
                 
+                                
                 
             </div>
 
