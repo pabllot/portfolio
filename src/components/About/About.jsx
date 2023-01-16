4f8c5e6503dc6787427552ef61e7d9035d2b0f85
Pabllot07 <pabllot07@outlook.com>
2023-01-16 10:37:32 -0300

    about desc

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,7 @@ const About = ({english}) => {
             </Wrapper>
             <Wrapper>                    
                 <TextBox>
-                    <Paragraph>{`${english ? "Hey there, my name is Pablo, I'm 25 years old. A frontend software developer and a series binge-watcher on my free time. Foccused and commited professional always aiming to achieve goals set and help my team." : 'Meu nome é Pablo Teixeira, tenho 25 anos. Sou um desenvolvedor frontend e maratonador de série nas horas vagas. Profissional dedicado e determinado a sempre atingir os objetivos propostos e ajudar sua equipe.'}`}</Paragraph>
+                    <Paragraph>{`${english ? "Hey there, my name is Pablo, I'm 25 years old. A frontend software developer and a series binge-watcher on my free time. Focused and commited professional always aiming to achieve goals that are set and help my team." : 'Meu nome é Pablo Teixeira, tenho 25 anos. Sou um desenvolvedor frontend e maratonador de série nas horas vagas. Profissional dedicado, determinado a sempre atingir os objetivos propostos e ajudar sua equipe.'}`}</Paragraph>
                 </TextBox>
             </Wrapper>
         </SubContainer>
