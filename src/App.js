33d8c8a28ce407b5a76865f9abac5342386d297d
Pabllot07 <pabllot07@outlook.com>
2023-04-16 10:53:20 -0300

    index /cleaning the code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react'
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
-import Skills from './components/Skills/Skills';
-import Work from './components/Work/Work';
-import { Container } from './styles';
+import React, { useState } from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import { Container } from "./styles";
 
 function App() {
-  const [english, setEnglish] = useState(false)
+  const [english, setEnglish] = useState(false);
+
   return (
     <Container>
       <Navbar english={english} setEnglish={setEnglish} />
-      <Home english={english} setEnglish={setEnglish} />
+      <Hero english={english} setEnglish={setEnglish} />
       <Skills english={english} setEnglish={setEnglish} />
       <Work english={english} setEnglish={setEnglish} />
       <About english={english} setEnglish={setEnglish} />
